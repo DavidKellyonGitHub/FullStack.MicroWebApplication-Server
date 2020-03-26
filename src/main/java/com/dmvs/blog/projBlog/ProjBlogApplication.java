d40b309b77ad5fe906608a3d44484652b7b77033
Dave Kelly <kelly.david.t@gmail.com>
2020-03-26 15:14:32 -0400

    init commit

---
+++ src/main/java/com/dmvs/blog/projBlog/ProjBlogApplication.java
@@ -0,0 +1,13 @@
+package com.dmvs.blog.projBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmvs/blog/projBlog/ProjBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmvs.blog.projBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
