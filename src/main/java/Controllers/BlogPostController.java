3797ba6ea833d051cf6ac707a9237c005a35af88
asconem <asconem@gmail.com>
2020-04-02 16:12:03 -0400

    Updating

--- src/main/java/Controllers/BlogPostController.java
+++ src/main/java/Controllers/BlogPostController.java
@@ -2,12 +2,16 @@ package Controllers;
 
 import Models.BlogPost;
 import Services.BlogPostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BlogPostController {
+
+    @Autowired
     BlogPostService blogPostService;
 
     @ResponseBody
