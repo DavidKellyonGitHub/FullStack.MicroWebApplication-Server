c6251cfb5071a01f22603ac56ddf919c435a211b
asconem <asconem@gmail.com>
2020-04-02 22:23:31 -0400

    Adding Comment test stubs

---
+++ src/test/java/Controllers/CommentControllerTest.java
@@ -0,0 +1,28 @@
+package Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentControllerTest {
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAllByBlogId() {
+    }
+
+    @Test
+    void saveComment() {
+    }
+
+    @Test
+    void updateComment() {
+    }
+
+    @Test
+    void deleteComment() {
+    }
+}
