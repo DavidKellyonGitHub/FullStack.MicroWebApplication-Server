0d5f9295005ba4be2bff8398cfd4f71ac39ac8b3
Dave Kelly <kelly.david.t@gmail.com>
2020-04-16 15:53:43 -0400

    generated blogpost and comment components

--- frontEnd/blog/src/app/app.module.ts
+++ frontEnd/blog/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { CommentComponent } from './comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule
